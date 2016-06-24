60316f75896a18c247c2be7b2bf9136bf41148f1
Juergen <juergen@ITT.com>
2016-06-24 13:39:38 +0200

    added dummy app

---
+++ Assignment_7/dummy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# -*- coding: utf-8 -*-
+
+COOL_GOT_QUOTE = "WHERE ARE MY DRAGONS"
+
+
+def main():
+    print(COOL_GOT_QUOTE + "(Called from other application")
+
+
+if __name__ == '__main__':
+    main()
