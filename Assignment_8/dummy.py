580fd675193a7bcbf2ec192b94a7f1b36a75ea6e
Juergen <juergen@ITT.com>
2016-07-01 06:29:38 +0200

    forgot dummy.py

---
+++ Assignment_8/dummy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# -*- coding: utf-8 -*-
+
+COOL_GOT_QUOTE = 'WHEN YOU PLAY THE GAME OF THRONES, YOU WIN OR YOU DIE.'
+
+
+def main():
+    print(COOL_GOT_QUOTE + "\nCalled from other application(dummy.py)")
+
+
+if __name__ == '__main__':
+    main()
